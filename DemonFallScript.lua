0960043d67d4bda537a4f8a24bbe8f832d21f7ce
约 - Misk <test133y@gmail.com>
2024-07-31 13:56:45 +0300

    DemonFallScript.lua

--- DemonFallScript.lua
+++ DemonFallScript.lua
@@ -1,18 +1,16 @@
--- استمع لحدث إسقاط العناصر من الحقيبة
 game.Players.LocalPlayer.Backpack.ChildRemoved:Connect(function(item)
-    -- تحقق إذا كان العنصر قد أُسقط
+    -- تحقق من أن العنصر أُسقط بالفعل من الحقيبة
     if not item:IsDescendantOf(game.Players.LocalPlayer.Backpack) then
         return
     end
     
-    -- اسم العنصر المُزال
-    local itemName = item.Name
-    local itemPosition = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
-
-    -- تكثير العنصر إلى 1000 ووضعهم في مكان العنصر المُسقط
-    for i = 1, 1000 do
+    -- مضاعفة العنصر إلى 1000 مرة
+    for i = 1, 999 do
         local clonedItem = item:Clone()
         clonedItem.Parent = workspace
-        clonedItem.Position = itemPosition + Vector3.new(math.random(-5, 5), 0, math.random(-5, 5))
+        clonedItem.Position = item.Position -- ضع النسخ في نفس مكان العنصر الأصلي
     end
+    
+    -- التأكد من تحديث الرقم ليظهر 1000 بدلاً من 1
+    item.Quantity = 1000
 end)
